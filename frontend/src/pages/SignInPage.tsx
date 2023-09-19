9a79c6f99c48499cea2f51539ae588b393dd518f
phat-eoh <phat.pht@eoh.io>
2023-09-19 16:27:38 +0700

    nav logo

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -1,38 +1,42 @@
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import SignInForm from "../components/auth/SignInForm";
 import Colors from "../constants/color";
-
+import { Col, Navbar as NB, Row } from "react-bootstrap";
 export default function SignInPage() {
-  return (
-    <div className="px-5 vh-100" style={{ backgroundColor: Colors.DarkBlue }}>
-      <Row className="mx-5 h-100 w-100 align-items-center">
-        <Col className="d-none d-lg-block col-lg-6">
-          <Image src="login.jpeg" className="w-100 m-5" />
-        </Col>
-        <Col className="d-flex flex-column align-items-center justify-content-center text-center">
-          <div
-            style={{
-              width: 400,
-              color: Colors.BabyBlue,
-              borderRadius: 20,
-              border: "2px solid",
-            }}
-          >
-            <div className="items-center my-5">
-              <img className="w-50" src="logo2.svg" />
-            </div>
-            <h1 className="text-3xl mb-4">SIGN IN</h1>
-            <div className="flex flex-row">
-              <p>
-                Don't have account ? <a href="/signup">Sign Up</a>
-              </p>
-            </div>
-            <SignInForm />
-          </div>
-        </Col>
-      </Row>
-    </div>
-  );
+    return (
+        <div
+            className="px-5 vh-100"
+            style={{ backgroundColor: Colors.DarkBlue }}
+        >
+            <Row className="mx-5 h-100 w-100 align-items-center">
+                <Col className="d-none d-lg-block col-lg-6">
+                    <Image src="login.jpeg" className="w-100 m-5" />
+                </Col>
+                <Col className="d-flex flex-column align-items-center justify-content-center text-center">
+                    <div
+                        style={{
+                            width: 400,
+                            color: Colors.BabyBlue,
+                            borderRadius: 20,
+                            border: "2px solid",
+                        }}
+                    >
+                        <div className="items-center my-5">
+                            <NB.Brand className="h-100" href="/">
+                                <img src="logo2.svg" className="h-100"></img>
+                            </NB.Brand>
+                        </div>
+                        <h1 className="text-3xl mb-4">SIGN IN</h1>
+                        <div className="flex flex-row">
+                            <p>
+                                Don't have account ?{" "}
+                                <a href="/signup">Sign Up</a>
+                            </p>
+                        </div>
+                        <SignInForm />
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    );
 }
--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,38 +1,43 @@
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Col, Navbar as NB, Row } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
 import SignUpForm from "../components/auth/SignUpForm";
 import Colors from "../constants/color";
 
 export default function SignUpPage() {
-  return (
-    <div className="px-5 vh-100" style={{ backgroundColor: Colors.DarkBlue }}>
-      <Row className="mx-5 h-100 w-100 align-items-center">
-        <Col className="d-none d-lg-block col-lg-6">
-          <Image src="signup.jpeg" className="w-100 m-5" />
-        </Col>
-        <Col className="d-flex flex-column align-items-center justify-content-center text-center">
-          <div
-            style={{
-              width: 400,
-              color: Colors.BabyBlue,
-              borderRadius: 20,
-              border: "2px solid",
-            }}
-          >
-            <div className="items-center my-5">
-              <img className="w-50" src="logo2.svg" />
-            </div>
-            <h1 className="text-3xl mb-4">SIGN UP</h1>
-            <div className="flex flex-row">
-              <p>
-                Already had an account ? <a href="/signin">Sign In</a>
-              </p>
-            </div>
-            <SignUpForm />
-          </div>
-        </Col>
-      </Row>
-    </div>
-  );
+    return (
+        <div
+            className="px-5 vh-100"
+            style={{ backgroundColor: Colors.DarkBlue }}
+        >
+            <Row className="mx-5 h-100 w-100 align-items-center">
+                <Col className="d-none d-lg-block col-lg-6">
+                    <Image src="signup.jpeg" className="w-100 m-5" />
+                </Col>
+                <Col className="d-flex flex-column align-items-center justify-content-center text-center">
+                    <div
+                        style={{
+                            width: 400,
+                            color: Colors.BabyBlue,
+                            borderRadius: 20,
+                            border: "2px solid",
+                        }}
+                    >
+                        <div className="items-center my-5">
+                            <NB.Brand className="h-100" href="/">
+                                <img src="logo2.svg" className="h-100"></img>
+                            </NB.Brand>
+                        </div>
+                        <h1 className="text-3xl mb-4">SIGN UP</h1>
+                        <div className="flex flex-row">
+                            <p>
+                                Already had an account ?{" "}
+                                <a href="/signin">Sign In</a>
+                            </p>
+                        </div>
+                        <SignUpForm />
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    );
 }
